151e2b7bb4b40e295bb8405528bd5dba7ce4fdc8
Aishaabdi1 <aishahajisirad@gmail.com>
2022-09-12 08:39:25 +0100

    Delete request with ID

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -60,7 +60,6 @@ router.put("/:id", (req, res) => {
       tag_name: req.body.tag_name,
     },
     {
-      where: {
         id: req.params.id,
       },
     }
@@ -72,9 +71,19 @@ router.put("/:id", (req, res) => {
       res.json(err);
     });
 });
-
 router.delete("/:id", (req, res) => {
-  // delete on tag by its `id` value
+	// delete on tag by its `id` value
+	Tag.destroy({
+			where: {
+				id: req.params.id,
+			},
+		})
+		.then((qtyRemoved) => {
+			res.json(`${qtyRemoved} tag were removed from the database`);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
 });
 
 module.exports = router;
