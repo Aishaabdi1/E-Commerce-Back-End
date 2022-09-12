3f99edf65818a4b252eeba04665b6a479dc74b96
Aishaabdi1 <aishahajisirad@gmail.com>
2022-09-12 16:30:37 +0100

    Delete request with id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -38,30 +38,43 @@ router.get("/:id", async (req, res) => {
 });
 
 // POST request: creates a new Category with the  request body data
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   await Category.create(req.body)
-		.then((newCategory) => res.status(200).json(newCategory))
-		.catch((err) => {
-			console.log(err);
-			res.status(400).json(err);
-		});
+    .then((newCategory) => res.status(200).json(newCategory))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 // PUT request with an id: updates this category with the request body data
-router.put('/:id', async (req, res) => {
+router.put("/:id", async (req, res) => {
   await Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((cat) => Category.findByPk(req.params.id))
+    .then((updatedCategory) => res.status(200).json(updatedCategory))
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+// DELETE request with id: destroys the data for the category with this id.
+router.delete('/:id', async (req, res) => {
+	await Category.destroy({
 		where: {
 			id: req.params.id,
 		},
 	})
-  .then(cat => Category.findByPk(req.params.id))
-  .then((updatedCategory) => res.status(200).json(updatedCategory))
-  .catch((err) => {res.json(err);});
+	.then((rmvdCategory) => {
+		res.json(`The category was removed from the database`);
+	})
+	.catch((err) => {
+		res.json(err);
+	});
 });
 
-
-router.delete("/:id", (req, res) => {
-  // delete a category by its `id` value
-});
 
 module.exports = router;
