067c23e9c79caea60a6e21d842bc200991e82e97
Aishaabdi1 <aishahajisirad@gmail.com>
2022-09-12 08:10:48 +0100

    Begin by creating Tag routes to return products based on tags.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,27 +1,41 @@
-const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../Develop/models');
+const router = require("express").Router();
+const { Tag, Product, ProductTag } = require("../../Develop/models");
 
-// The `/api/tags` endpoint
-
-router.get('/', (req, res) => {
-  // find all tags
+// Endpoint for all the `/api/tags` requests
+router.get("/", async (req, res) => {
+  // respond with all tags: GET request without id
   // be sure to include its associated Product data
+  await Tag.findAll({
+    attributes: ["id", "tag_name"],
+    include: [
+      {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id"],
+        through: "ProductTag",
+      },
+    ],
+  })
+    .then((parsedTagData) => {
+      res.json(parsedTagData);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
-
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // create a new tag
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete on tag by its `id` value
 });
 
